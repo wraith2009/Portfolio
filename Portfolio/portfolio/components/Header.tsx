29a9c720a471b54ea0aeccdc31b90e22997b7861
Rahul Bhardwaj <rbh97995@gmail.com>
2024-09-11 11:46:23 +0530

    Refactor component imports to use new magicui directory structure

--- Portfolio/portfolio/components/Header.tsx
+++ Portfolio/portfolio/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import ShineBorder from "./shineBorder";
+import ShineBorder from "./magicui/shine-border";
 
 const Header: React.FC = () => {
   return (
