d383654bd8345dc9edcc47deab1cb1dfdcd727ef
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-10-17 21:40:38 +0530

    Add @vercel/analytics package and integrate analytics tracking in layout component

--- Portfolio/portfolio/app/layout.tsx
+++ Portfolio/portfolio/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SocialDock from "@/components/connect";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <SocialDock />
         {children}
+        <Analytics />
       </body>
     </html>
   );
