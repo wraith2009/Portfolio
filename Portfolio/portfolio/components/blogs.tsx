830e36a335e8cb876fb7f0f2d326b1c7bdf7301d
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2025-05-25 17:47:11 +0530

    feat: fixed colon

--- Portfolio/portfolio/components/blogs.tsx
+++ Portfolio/portfolio/components/blogs.tsx
@@ -17,18 +17,21 @@ export default function Blogs_section() {
       link: "https://medium.com/@rbh97995/eventual-consistency-in-distributed-systems-a-real-world-perspective-1175fd0f742b",
     },
   ];
+
   return (
-    <div className=" mt-4 px-4 md:px-16">
+    <div className="mt-4 px-4 md:px-16">
       <div className="my-20">
         <div className="flex items-center gap-2 mb-2">
           <h2 className="text-3xl font-bold text-white">Recent blog posts</h2>
         </div>
-        <p className="text-gray-400 mb-6">Things I have written recently.</p>s
+        <p className="text-gray-400 mb-6">Things I have written recently.</p>
         <ul className="space-y-4 list-none">
           {blogs.map((blog, index) => (
-            <li key={index} className="flex items-baseline gap-2">
+            <li key={index} className="flex items-baseline gap-2 flex-wrap">
               <span className="text-gray-400">•</span>
-              <span className="text-gray-500 w-28">{blog.date} : </span>
+              <span className="text-gray-500 w-fit whitespace-nowrap">
+                {blog.date} :
+              </span>
               <a
                 href={blog.link}
                 target="_blank"
