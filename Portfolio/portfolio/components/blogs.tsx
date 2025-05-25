e1659aab987aa0ddc965000a1b2a38217d79b010
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2025-05-25 17:44:18 +0530

    feat: added my new blog on distributed systems

--- Portfolio/portfolio/components/blogs.tsx
+++ Portfolio/portfolio/components/blogs.tsx
@@ -10,6 +10,12 @@ export default function Blogs_section() {
       title: "Understanding Phantom Reads: Ghosts in Your Database",
       link: "https://medium.com/@rbh97995/understanding-phantom-reads-ghosts-in-your-database-2e2964caf96b",
     },
+    {
+      date: "May 25, 2025",
+      title:
+        "Eventual Consistency in Distributed Systems: A Real-World Perspective",
+      link: "https://medium.com/@rbh97995/eventual-consistency-in-distributed-systems-a-real-world-perspective-1175fd0f742b",
+    },
   ];
   return (
     <div className=" mt-4 px-4 md:px-16">
@@ -17,8 +23,7 @@ export default function Blogs_section() {
         <div className="flex items-center gap-2 mb-2">
           <h2 className="text-3xl font-bold text-white">Recent blog posts</h2>
         </div>
-        <p className="text-gray-400 mb-6">Things I have written recently.</p>
-
+        <p className="text-gray-400 mb-6">Things I have written recently.</p>s
         <ul className="space-y-4 list-none">
           {blogs.map((blog, index) => (
             <li key={index} className="flex items-baseline gap-2">
